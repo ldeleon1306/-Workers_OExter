a76afbf86cd36e2d0cffd0624a9dcc944205bc05
ldeleon1306 <45015209+ldeleon1306@users.noreply.github.com>
2023-02-23 17:46:27 -0300

    Add files via upload

---
+++ Mail.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace WorkerS_OExtern.Mail
+{
+    public class Mail
+    {
+        private readonly IConfiguration _configuration;
+        private string _mailTo { get; set; }
+        private string _mailFrom { get; set; }
+        private string _smtpServer { get; set; }
+
+        public Mail(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _mailTo = _configuration["MailSmtp:mailTo"];
+            _mailFrom = _configuration["MailSmtp:mailFrom"];
+            _smtpServer = _configuration["MailSmtp:smtp"];
+        }
+
+        public void SendEmail(string pathArchivo, string cliente)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(_mailFrom);
+                var multiple = _mailTo.Split(';');
+                foreach (var to in multiple)
+                {
+                    if (to != string.Empty)
+                        mail.To.Add(to);
+                }
+                //System.Net.Mail.Attachment attachment;
+                //attachment = new System.Net.Mail.Attachment(pathArchivo);
+                //mail.Attachments.Add(attachment);
+                //if (!String.IsNullOrEmpty(txterror))
+                //{
+                //  attachment = new System.Net.Mail.Attachment(txterror);
+                //  mail.Attachments.Add(attachment);
+                //}
+
+                string subject = string.Format($"WebService Serializacion {cliente} Recepción");
+                string bodyMsg = string.Format($"Se procesó un archivo xml, del cliente {cliente}");
+                mail.Subject = subject;
+                mail.Body = bodyMsg;
+                mail.IsBodyHtml = true;
+
+                SmtpClient smtp = new SmtpClient(_smtpServer);
+                smtp.EnableSsl = false;
+                smtp.Port = 25;
+                smtp.UseDefaultCredentials = true;
+                smtp.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError(ex, "Se produjo una excepción en el metodo SendEmail: ", ex.Message);
+            }
+        }
+    }
+}
---
+++ WorkerFilterCheck.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using Serilog;
+using Microsoft.Extensions.Configuration;
+
+namespace WorkerS_OExtern
+{
+    public class WorkerFilterCheck : BackgroundService
+    {
+        private readonly ILogger<WorkerFilterCheck> _logger;
+        private readonly IConfiguration _configuration;
+
+        public WorkerFilterCheck(ILogger<WorkerFilterCheck> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Log.Logger = new LoggerConfiguration().WriteTo.File(@"C:\Users\ldeleon\OneDrive - ANDREANI LOGISTICA SA\Documentos\Bp\vane caso tarejta naranja\EventosLog\LogEvents" + DateTime.Now.ToString("ddMMyyyyHHmmss") + ".log").CreateLogger();
+                CompareAsync();
+                await Task.Delay(9000000, stoppingToken);
+            }
+        }
+
+        private async Task CompareAsync()
+        {
+            try
+            {
+                var client = new MongoClient("mongodb://10.20.2.46:27017");
+                List<string> NombrebaseDatos = client.ListDatabaseNames().ToList();
+                var database = client.GetDatabase("APIAlmacenes");
+
+                var collection = database.GetCollection<BsonDocument>("TransaccionesPedidos");
+                int cont = 0;
+                //using (StreamReader leer = new StreamReader(@"C:\Users\ldeleon\OneDrive - ANDREANI LOGISTICA SA\Documentos\Bp\vane caso tarejta naranja\ordenExt.txt"))
+                using (StreamReader leer = new StreamReader(@"C:\Users\ldeleon\OneDrive - ANDREANI LOGISTICA SA\Documentos\Bp\vane caso tarejta naranja\idtran.txt"))
+                {
+                    while (!leer.EndOfStream)
+                    {
+                        string url = leer.ReadLine();
+
+                        try
+                        {
+                            var list = await collection.Find(new BsonDocument())
+                        .Limit(2) //retrive only two documents
+                        .ToListAsync();
+                            foreach (var docs in list)
+                            {
+                                Console.WriteLine(docs);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+
+
+                        }
+                        break;
+
+                    }
+                }
+                Mail.Mail m = new Mail.Mail(_configuration);
+                m.SendEmail("nada", "cliente");
+                Console.WriteLine(cont);
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}

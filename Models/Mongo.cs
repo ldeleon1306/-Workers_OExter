eb084118b7dc46f84ed1e44ce24257a0b76a2523
ldeleon1306 <45015209+ldeleon1306@users.noreply.github.com>
2023-03-01 00:46:55 -0300

    Add files via upload

--- Models/Mongo.cs
+++ Models/Mongo.cs
@@ -17,7 +17,7 @@ namespace Workers.Models
             try
             {
                 Console.WriteLine("entra a mongo 19");
-                var client = new MongoClient("mongodb://10.20.2.46:27017?connect=replicaSet");
+                var client = new MongoClient("mongodb://10.20.2.47:27017?connect=replicaSet");
                 Console.WriteLine("conecto a mongo 21");
                 Console.WriteLine("entra a mongo 22");
                 List<string> NombrebaseDatos = client.ListDatabaseNames().ToList();

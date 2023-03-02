1c12c3e69cc808038ca3546debec1ff72a0f7b53
ldeleon1306 <45015209+ldeleon1306@users.noreply.github.com>
2023-03-02 18:11:10 -0300

    Add files via upload

---
+++ Wap_IngresosPedidosContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerConecMongo
+{
+    class Wap_IngresosPedidosContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //"Server=sql_server2022;Database=SalesDb;User Id=SA;Password=A&VeryComplex123Password;MultipleActiveResultSets=true"
+            //optionsBuilder.UseSqlServer("Server=DBINTERFWHSDESA;Database=InterfacesWHS;User Id=User_InterfacesWHS;Password=0jIN7HD9BlT6cnGoKZYa;MultipleActiveResultSets=true;");
+            //optionsBuilder.UseSqlServer("Data Source=DBINTERFWHSDESA;Initial Catalog=InterfacesWHS;User ID=User_InterfacesWHS;Password=0jIN7HD9BlT6cnGoKZYa;Integrated Security=False;");
+            //optionsBuilder.UseSqlServer(@"Data Source=SQLSCECYPESRV.andreani.com.ar;Initial catalog=InterfacesWHS;Integrated Security=true");
+            //"Data Source=%DNS%.andreani.com.ar;Initial Catalog=%base%;Persist Security Info=True;User ID=%User%;Password=%Password%"
+                  optionsBuilder.UseSqlServer(@"Data Source=DBINTERFWHSDESA.andreani.com.ar;Initial catalog=InterfacesWHS;User ID=User_InterfacesWHS;Password=0jIN7HD9BlT6cnGoKZYa;");
+            //optionsBuilder.UseSqlServer(@"Data Source=SQLSCECYPESRV;Initial catalog=InterfacesWHS;Integrated Security=true");
+            //optionsBuilder.UseSqlServer(@"Data Source=ITGDESAOCSRV;Initial catalog=AccionesBBVA;Integrated Security=true");
+        }
+
+        public DbSet<WAP_INGRESOPEDIDOS> WAP_INGRESOPEDIDOS { get; set; }
+    }
+}
--- Worker.cs
+++ Worker.cs
@@ -1,15 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading;
 using System.Threading.Tasks;
-using MongoDB.Driver;
-using MongoDB.Bson;
-using System.IO;
 
-namespace WorkerS_OExtern
+namespace WorkerConecMongo
 {
     public class Worker : BackgroundService
     {
@@ -25,57 +26,118 @@ namespace WorkerS_OExtern
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                CompareAsync();
-                await Task.Delay(100000, stoppingToken);
-            }
-        }
 
-        private async Task CompareAsync()
-        {
-            try
-            {
-                var client = new MongoClient("mongodb://10.20.7.44:27017");
-                List<string> NombrebaseDatos = client.ListDatabaseNames().ToList();
-                var database = client.GetDatabase("APIAlmacenes");
+                //Context.Wap_IngresosPedidosContexts s = new Context.Wap_IngresosPedidosContexts();
+                //foreach (var item in s.WapIngresopedidos.Take(10).ToList())
+                //{
+                //    Console.WriteLine(item.OrdenExterna1);
+                //}
 
-                var collection = database.GetCollection<BsonDocument>("TransaccionesPedidos");
-                int cont = 0;
-                    //using (StreamReader leer = new StreamReader(@"C:\Users\ldeleon\OneDrive - ANDREANI LOGISTICA SA\Documentos\Bp\vane caso tarejta naranja\ordenExt.txt"))
-                using (StreamReader leer = new StreamReader(@"C:\Users\ldeleon\OneDrive - ANDREANI LOGISTICA SA\Documentos\Bp\vane caso tarejta naranja\idtran.txt"))
+
+                //            var client = new MongoClient("mongodb://10.20.7.44:27017");
+                //            Console.WriteLine("conecto a mongo 20");
+                //            Console.WriteLine("entra a mongo 22");
+                //            List<string> NombrebaseDatos = client.ListDatabaseNames().ToList();
+                //            Console.WriteLine("entra a mongo 23");
+                //            var database = client.GetDatabase("APIAlmacenes");
+                //            Console.WriteLine(database);
+
+                //            List<CollectionMongo> listRange = new List<CollectionMongo>();
+                //            var collection = database.GetCollection<BsonDocument>("TransaccionesPedidos");
+                //            var list = collection.Find(new BsonDocument())
+                //               .Limit(2) //retrive only two documents
+                //              .ToList();
+
+                //            foreach (var docs in list)
+                //            {
+                //                //_logger.LogInformation(docs.ToString());
+                //                //Console.WriteLine("Idtransaccion: " + docs["response"]["idtransaccion"] + "  Estado: " + docs["estado"]);
+
+                //                listRange.Add(new CollectionMongo() { idtransaccion = (int)Convert.ToInt64(docs["response"]["idtransaccion"]), estado = (string)docs["estado"] });
+                //                //_logger.LogInformation(docs.ToString());
+                //                Console.WriteLine(docs);
+                //            }
+
+                Wap_IngresosPedidosContext db = new Wap_IngresosPedidosContext();
+                WAP_INGRESOPEDIDOS wp = new WAP_INGRESOPEDIDOS();
+                var idlist = new int[] { 2, 3 };
+                Console.WriteLine("antes  raw sql");
+
+                var students = db.WAP_INGRESOPEDIDOS
+                  .FromSqlRaw("SELECT * FROM [InterfacesWHS ].[dbo].[WAP_INGRESOPEDIDOS]")
+    .Take(10)
+    .ToList();
+
+                Console.WriteLine("despues  raw sql");
+                Console.WriteLine(students.Count());
+
+
+                //var estados = from p in db.WAP_INGRESOPEDIDOS
+                //              where p.IdTransacción == "660" && idlist.Contains(p.Estado)
+                //              select new WAP_INGRESOPEDIDOS()
+                //              {
+                //                  IdTransacción = p.IdTransacción,
+                //                  Estado = p.Estado,
+                //                  Propietario = p.Propietario,
+                //                  RazonFalla = p.RazonFalla,
+                //                  Almacen = p.Almacen,
+                //                  OrdenExterna1 = p.OrdenExterna1
+                //              };
+
+                //Console.WriteLine("contar estados");
+                //int a = 0;
+                //foreach (var item in estados)
+                //{
+                //    a++;
+                //}
+                //    Console.WriteLine("estados");
+                //    Console.WriteLine(a);
+
+                try
                 {
-                        while (!leer.EndOfStream)
-                        {
-                            string url = leer.ReadLine();
+                    MailMessage mail = new MailMessage();
+                    //Console.WriteLine(_mailFrom.ToString());
+                    //Console.WriteLine(_mailTo);
+                    mail.From = new MailAddress("apptestbrdcsrv@andreani.com");
+                    mail.To.Add("ldeleon@andreani.com");
+                    //var multiple = _mailTo.Split(';');
+                    //foreach (var to in multiple)
+                    //{
+                    //    if (to != string.Empty)
+                    //        mail.To.Add(to);
+                    //}              
+                    System.Net.Mail.Attachment attachment;
+                    //attachment = new System.Net.Mail.Attachment("output.txt");
+                    //mail.Attachments.Add(attachment);
+                    //if (!String.IsNullOrEmpty(txterror))
+                    //{
+                    //  attachment = new System.Net.Mail.Attachment(txterror);
+                    //  mail.Attachments.Add(attachment);
+                    //}
+                    string cliente = "prueba";
+                    string subject = string.Format($"Prueba {cliente} Mail");
+                    string bodyMsg = string.Format($"Se procesó un mongo {cliente}");
+                    mail.Subject = subject;
+                    mail.Body = bodyMsg;
+                    mail.IsBodyHtml = true;
+                    Console.WriteLine(bodyMsg);
+                    SmtpClient smtp = new SmtpClient("10.20.7.16");
+                    smtp.EnableSsl = false;
+                    smtp.Port = 25;
+                    smtp.UseDefaultCredentials = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("dentro de casa mail");
+                    //Console.WriteLine(_mailFrom);
+                    //Console.WriteLine(_mailTo);
+                    Console.WriteLine(ex.Message);
+                    //_logger.LogError(ex, "Se produjo una excepción en el metodo SendEmail: ", ex.Message);
+                    throw ex;
+                }
+               
 
-                        try
-                        {
-                            //var filter = Builders<BsonDocument>.Filter.Eq("response.idtransaccion",Convert.ToInt32(url));
-                            var filter = Builders<BsonDocument>.Filter.Eq("response.idtransaccion", Convert.ToInt32(url));
-
-                            var document = collection.Find(filter).First();
-
-                            Console.WriteLine("Idtransaccion: " + document["response"]["idtransaccion"]  +"  Razon: " + document["razon"]);
-                            //Console.WriteLine(document["response"]["idtransaccion"]);
-                            //{ "request.pedido.idpedido": "coc5i0y2fqhw"}
-                            cont++;
-                        }
-                        catch (Exception ex)
-                        {
-
-                           
-                        }
-                               
-                            
-                        }
-                    }
-
-
-                Console.WriteLine(cont);
-
-            }
-            catch (Exception ex)
-            {
-                throw;
+                await Task.Delay(300000, stoppingToken);
             }
         }
     }
